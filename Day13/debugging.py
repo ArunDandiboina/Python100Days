b7dc088247223b870897b61e6454e6d23396a48b
Arun Dandiboina <d.arun672002@gmail.com>
2025-06-18 13:13:50 +0530

    Complete Day13 - Debugging

---
+++ Day13/debugging.py
@@ -0,0 +1,22 @@
+# Debugging - The process of removing bugs from the code.
+
+# 1. Describing the problem.
+### what is the problem?
+### what are the assumptions?
+### what is the expected output?
+
+# 2. Reproducing the problem.
+### see the error message.
+### run the code with different inputs.
+
+# 3. play like computer. line by line. input and output.
+
+# 4. Notice and Fix errors produced by the editor. (red)
+
+# 5. Use print statements.
+
+# 6. Use a debugger.
+
+# 7. AI
+
+# 8. Ask for help.
